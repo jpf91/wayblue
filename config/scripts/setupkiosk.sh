19fad8536a833fb21d6f6d937dd9e603e2596fc7
Johannes Pfau <johannespfau@gmail.com>
2024-02-18 16:33:57 +0100

    Kiosk implementation files

---
+++ config/scripts/setupkiosk.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# Tell this script to exit if there are any errors.
+# You should have this in every custom script, to ensure that your completed
+# builds actually ran successfully without any errors!
+set -oue pipefail
+
+#systemctl disable gdm
+systemctl enable sway-kiosk.service
+
+useradd -ms /bin/bash kiosk
+systemctl -M kiosk@ --user enable chromium-kiosk.service
+systemctl -M kiosk@ --user enable squeekboard.service
+sudo -u kiosk mkdir -p /home/kiosk/.config/sway/
+sudo -u kiosk cp /etc/sway/config.kiosk /home/kiosk/.config/sway/config
